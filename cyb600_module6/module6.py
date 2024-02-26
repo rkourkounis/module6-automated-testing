e622df69c6e899659ab95e0a7682eeb0deda5504
rkourkounis <158241525+rkourkounis@users.noreply.github.com>
2024-02-26 13:41:00 -0500

    Update module6.py

--- cyb600_module6/module6.py
+++ cyb600_module6/module6.py
@@ -1,13 +1,5 @@
 def only_odd_numbers(arr):
-    if not isinstance(arr, list):
-        return []
-    # iterating each number in list
-    output_arr = []
-    for num in arr:
-        # checking condition
-        if num % 2 != 0:
-            output_arr.append(num)
-    return output_arr
+    pass
 
 def only_even_numbers(arr):
     if not isinstance(arr, list):
